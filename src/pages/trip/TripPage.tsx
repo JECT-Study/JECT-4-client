ddfca0a165c840a98c3d6654ab91d1f0f50129b8
ccconac <qwerpoiu91@naver.com>
2025-08-14 14:40:12 +0900

    refactor: refactor goal step component and remove unnecessary parameter

--- src/pages/trip/TripPage.tsx
+++ src/pages/trip/TripPage.tsx
@@ -5,23 +5,17 @@ import GoalStep from './_components/GoalStep';
 const TripPage = () => {
     const navigate = useNavigate();
 
-    const path1 = 'M118 0V66C118 80 106 92 92 92H31C16 92 5 103 5 118V150';
-    const path2 = 'M5 0V5.5C5 15 13 23 23 26L211 26C225 26 237 38 237 50V50';
-
-    const steps = [
+    const mockSteps = [
         {
             sequence: '01',
             title: '문제 유형 파악',
             align: 'center',
-            pathD: path1,
-            svgHeight: 120,
             goalState: 'complete',
         },
         {
             sequence: '02',
             title: '문제 유형 파악',
             align: 'left',
-            pathD: path2,
             goalState: 'complete',
         },
         {
@@ -29,7 +23,6 @@ const TripPage = () => {
             title: `유형 연습\nQ8-10 복습`,
             align: 'right',
             pathDirection: 'flipped',
-            pathD: path2,
             goalState: 'goal',
             onNavigate: () => navigate('/trip/dashboard'),
         },
@@ -37,20 +30,17 @@ const TripPage = () => {
             sequence: '04',
             title: '문제 유형 파악',
             align: 'left',
-            pathD: path2,
         },
         {
             sequence: '05',
             title: '문제 유형 파악',
             align: 'right',
             pathDirection: 'flipped',
-            pathD: path2,
         },
         {
             sequence: '06',
             title: '문제 유형 파악',
             align: 'left',
-            pathD: path2,
             isLast: true,
         },
     ] as const;
@@ -61,9 +51,8 @@ const TripPage = () => {
             <div className="h-[4rem]">
                 <BackHeader title="토익 뿌시기" hideLogButton={false} />
             </div>
-
             <div className="h-[calc(100vh-10rem)] overflow-y-auto pt-6 [&::-webkit-scrollbar]:hidden">
-                {steps.map((step) => (
+                {mockSteps.map((step) => (
                     <GoalStep key={step.sequence} {...step} />
                 ))}
             </div>
