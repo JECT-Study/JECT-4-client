eb9747b22dd34ceff0cc82e0f8744854e3145a52
pjyoung0511 <pjiyoung4512@gmail.com>
2025-08-30 17:44:11 +0900

    fix : 회원가입 후 여행 생성 페이지로 이동

--- src/pages/setup/setProfile/SetJobPage.tsx
+++ src/pages/setup/setProfile/SetJobPage.tsx
@@ -37,7 +37,7 @@ function SetJobPage() {
             // 3. 성공 시 메인 페이지로 이동
             localStorage.setItem('accessToken', response.data.accessToken);
             localStorage.setItem('refreshToken', response.data.refreshToken);
-            navigate('/main', { replace: true });
+            navigate('/set-travel-type', { replace: true });
         } catch (error) {
             console.error('API 호출 실패:', error);
             alert('서버 요청 중 오류가 발생했습니다.');
