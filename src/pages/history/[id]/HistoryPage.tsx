a7e870989fdd7e67973b3b68b41ab785d0b4d3ab
ccconac <qwerpoiu91@naver.com>
2025-10-02 00:34:43 +0900

    feat: history page UI 구현

--- src/pages/history/[id]/HistoryPage.tsx
+++ src/pages/history/[id]/HistoryPage.tsx
@@ -1,10 +1,21 @@
+import Header from '../_componenets/Header';
 import MissionHistory from '../../../components/history/MissionHistory';
-import { history } from '../../../mocks/history';
+
+import { history, goalCardContents } from '../../../mocks/history';
+import { FirstSection, SecondSection, ThirdSection } from '../_layouts/';
 
 const HistoryPage = () => {
     return (
         <div>
-            <MissionHistory historyList={history} />
+            <Header text="조별 과제 PPT 만들기" />
+            <div className="flex h-[calc(100vh-8rem)] flex-col gap-6 overflow-auto [&::-webkit-scrollbar]:hidden">
+                <FirstSection goalCardContents={goalCardContents} />
+                <SecondSection />
+                <ThirdSection />
+                <section>
+                    <MissionHistory historyList={history} />
+                </section>
+            </div>
         </div>
     );
 };
