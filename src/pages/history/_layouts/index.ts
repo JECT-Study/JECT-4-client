c0f61576e078a8c552dff0ceca3bc4698233a88b
ccconac <qwerpoiu91@naver.com>
2025-10-02 00:34:30 +0900

    feat: third section UI 구현

--- src/pages/history/_layouts/index.ts
+++ src/pages/history/_layouts/index.ts
@@ -1,2 +1,3 @@
 export { default as FirstSection } from '../_layouts/FirstSection';
 export { default as SecondSection } from '../_layouts/SecondSection';
+export { default as ThirdSection } from '../_layouts/ThirdSection';
