689200a9fdbd8de790305dcb9ca01132116164bc
ccconac <qwerpoiu91@naver.com>
2025-10-02 00:34:04 +0900

    feat: first section UI 구현

---
+++ src/pages/history/_layouts/FirstSection.tsx
@@ -0,0 +1,36 @@
+import GoalCard, { type GoalCardProps } from '../_componenets/GoalCard';
+
+type GoalCardContentsType = {
+    [K in GoalCardProps['type']]: number;
+};
+
+interface FirstSectionProps {
+    goalCardContents: GoalCardContentsType;
+}
+
+const FirstSection = ({ goalCardContents }: FirstSectionProps) => {
+    return (
+        <section className="flex flex-col gap-2">
+            <div className="flex justify-end">
+                <p className="text-text-min text-caption">
+                    2025년 6월 4일 ~ 2025년 6월 10일
+                </p>
+            </div>
+            <div className="flex w-full gap-1.5">
+                {Object.keys(goalCardContents).map((keyAsString, index) => {
+                    const type = keyAsString as keyof GoalCardContentsType;
+
+                    return (
+                        <GoalCard
+                            type={type}
+                            key={index}
+                            goal={goalCardContents[type]}
+                        />
+                    );
+                })}
+            </div>
+        </section>
+    );
+};
+
+export default FirstSection;
