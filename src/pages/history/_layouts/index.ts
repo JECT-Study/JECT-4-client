ecfe84a9223ba3d5d2740f27de7caa8ae1d4d7fc
ccconac <qwerpoiu91@naver.com>
2025-10-02 01:14:38 +0900

    fix: index.ts 절대 경로 → 상대 경로 수정

--- src/pages/history/_layouts/index.ts
+++ src/pages/history/_layouts/index.ts
@@ -1,3 +1,3 @@
-export { default as FirstSection } from '../_layouts/FirstSection';
-export { default as SecondSection } from '../_layouts/SecondSection';
-export { default as ThirdSection } from '../_layouts/ThirdSection';
+export { default as FirstSection } from './FirstSection';
+export { default as SecondSection } from './SecondSection';
+export { default as ThirdSection } from './ThirdSection';
