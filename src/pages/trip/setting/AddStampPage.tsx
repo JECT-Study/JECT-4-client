d5327c55fbb1706f10b13b5243a74056a7921ae7
ccconac <qwerpoiu91@naver.com>
2025-10-13 01:37:28 +0900

    feat: 선형 스탬프 추가 페이지(ST-02) UI 구현

--- src/pages/trip/setting/AddStampPage.tsx
+++ src/pages/trip/setting/AddStampPage.tsx
@@ -1,7 +1,49 @@
+import MainButton from '../../../components/common/button/MainButton';
+import BackHeader from '../../../components/common/BackHeaderLayout';
+import Calendar from './_components/Calendar';
+
 const AddStampPage = () => {
     return (
-        <div>
-            <div>스탬프 추가 페이지</div>
+        <div className="flex flex-col gap-3">
+            <div className="h-[4rem]">
+                <BackHeader title="스탬프 추가" />
+            </div>
+            <section className="flex flex-col gap-2">
+                <span className="text-body text-text-min">스탬프 이름</span>
+                <input
+                    id="stamp-name"
+                    className="border-input-sub placeholder:text-subtitle focus:outline-input-sub h-[3.1875rem] rounded-md border bg-[#F8F7F5] px-4 py-3 placeholder:text-[rgba(117,117,117,0.40)] focus:bg-white"
+                    placeholder="문제 유형 파악"
+                />
+            </section>
+            <section className="flex flex-col gap-2 pt-6">
+                <span className="text-body text-text-min">마감일</span>
+                <div className="flex items-center gap-2">
+                    <input
+                        type="checkbox"
+                        className="accent-text-sub h-[1.375rem] w-[1.375rem]"
+                    />
+                    <p className="text-text-min text-body">마감일 없음</p>
+                </div>
+            </section>
+            <section className="flex flex-col gap-3 pt-5">
+                <div>
+                    <p className="text-text-min text-body">
+                        날짜를 선택해 주세요
+                    </p>
+                    <p className="text-point1 text-small">
+                        *여행 기간 내에서만 마감일을 설정할 수 있어요.
+                    </p>
+                </div>
+                <Calendar year={2025} month={6} />
+            </section>
+            <div className="pt-3">
+                <MainButton
+                    onClick={() => console.log('버튼 클릭')}
+                    children="저장"
+                    colorClass="bg-text-sub"
+                />
+            </div>
         </div>
     );
 };
