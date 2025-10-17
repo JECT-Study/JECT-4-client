cc89b6cbbd79deb13c8ea37ce532a1b92e990bc9
ccconac <qwerpoiu91@naver.com>
2025-10-17 21:32:16 +0900

    feat: 여행 리포트 상세 조회 API 요청 함수 구현

--- src/services/report/report.ts
+++ src/services/report/report.ts
@@ -15,3 +15,25 @@ export const getTripReport = async () => {
         );
     }
 };
+
+export const getDetailTripReport = async (
+    tripReportId: number,
+    page: number,
+    size: number
+) => {
+    try {
+        const { data } = await api.get(
+            `/trip-reports/${tripReportId}?page=${page}&size=${size}`
+        );
+
+        return data.data;
+    } catch (error: unknown) {
+        if ((error as any).response?.status === 404) {
+            throw new Error('상세 여행 리포트를 불러올 수 없습니다.');
+        }
+
+        throw new Error(
+            '상세 여행 리포트를 불러오지 못했습니다.  잠시 후 다시 시도해 주세요.'
+        );
+    }
+};
