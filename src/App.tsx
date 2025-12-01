b5046eb0c8a76d59f369373ac8a5e3f50508940a
ccconac <qwerpoiu91@naver.com>
2025-12-01 22:13:44 +0900

    feat: 최상위(App)에서 토큰 검증

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,39 @@
-import { BrowserRouter } from 'react-router';
-import Router from './routes.tsx';
-import './index.css';
+import { useEffect } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { getDefaultStore } from 'jotai';
 
+import { accessTokenAtom } from '@store/auth';
+import Router from './routes';
+import Loading from './components/common/Loading';
+import useTokenAutoRefresh from './hooks/token/useTokenAutoRefresh';
+import useAuthInitialization from './hooks/token/useAuthInitialization';
+
+/**
+ * 메인 App 컴포넌트
+ */
 function App() {
+    const store = getDefaultStore();
+
+    const { authReady } = useAuthInitialization();
+    const { scheduleTokenRefresh } = useTokenAutoRefresh();
+
+    useEffect(() => {
+        const unsubscribe = store.sub(accessTokenAtom, () => {
+            const newToken = store.get(accessTokenAtom);
+
+            if (newToken) scheduleTokenRefresh(newToken);
+        });
+
+        return () => {
+            unsubscribe();
+        };
+    }, [scheduleTokenRefresh]);
+
+    if (!authReady) return <Loading />;
+
     return (
         <BrowserRouter>
-            <div>
-                <Router />
-            </div>
+            <Router />
         </BrowserRouter>
     );
 }
